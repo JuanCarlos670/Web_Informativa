550a6df1f79cc25a32a484b6efcf51c0ddabbd9d
JuanCarlos670 <juancarlosqc17@gmail.com>
2025-01-06 22:09:09 -0500

    Agregar

--- ejer1.js
+++ ejer1.js
@@ -1,8 +1,10 @@
-let a= 41
-let b= 8
-let c= 10
+let a =parseInt(prompt("Ingrese el número a"))
+let b =parseInt(prompt("Ingrese el número b"))
+let c =parseInt(prompt("Ingrese el número c"))
 if(a>=b && a>=c){
     console.log("El mayor es: ",a)
 }else if(b>=c && b>=a){
     console.lov("El mayor es: ",b)
+}else{
+    console.log("El mayor es: ",c)
 }
